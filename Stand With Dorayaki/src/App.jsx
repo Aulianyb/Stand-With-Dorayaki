60347e808e0eb31f3a4024c1b04d292725cd0413
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2023-06-03 11:28:25 +0700

    Setup

---
+++ Stand With Dorayaki/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Dashboard() {
+  return (<>
+    <div className='Dashboard'>
+      <div>
+        <a>Home</a>
+      </div>
+    </div>
+  </>); 
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
