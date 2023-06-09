021efd5d09e02504868dc9320f8c8c1b170b89cb
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2023-06-09 13:57:47 +0700

    save dulu aja lah

--- Stand With Dorayaki/src/App.jsx
+++ Stand With Dorayaki/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Router, Link } from 'react-router-dom';
+
 
 function Dashboard() {
   return (<>
     <div className='dashboard'>
       <div>
-        <div className='dashboard-content'><a>Link</a></div>
-        <div className='web-name'>Stand With Dorayaki</div>
+        <div className='dashboard-content'>
+        {/* TO DO */}
+        </div>
+        <div className='web-name'>StandWithDorayaki</div>
       </div>
     </div>
   </>); 
@@ -17,8 +19,34 @@ function Dashboard() {
 function Banner(){
   return (
     <>
-      <div>
-        <img src='src/assets/Banner Temp.jpg' className='banner'></img>
+      <div className='banner-container'>
+        <img src='src/assets/Banner Temp.jpg' className='banner-img'></img>
+        <div className='store-name'>NAMA TOKO</div>
+      </div>
+    </>
+  );
+}
+
+function MenuBox(){
+  return (
+    <>
+        <div className='menu-box'> 
+          <img src='src/assets/Dorayaki.jpeg'></img>
+          <p>Rasa Dorayaki</p>
+          <p>Stock : x</p>
+        </div>
+    </>
+  );
+}
+
+function Menu(){
+  return (
+    <>
+      <div className='menu-container'>
+        <MenuBox />
+        <MenuBox />
+        <MenuBox />
+        <MenuBox />
       </div>
     </>
   );
@@ -26,11 +54,11 @@ function Banner(){
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <Dashboard />
       <Banner />
+      <Menu />
     </>
   )
 }
--- Stand With Dorayaki/vite.config.js
+++ Stand With Dorayaki/vite.config.js
@@ -1,7 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'public')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir : true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        nested: resolve(root, 'about', 'index.html'),
+      },
+    },
+  },
 })
